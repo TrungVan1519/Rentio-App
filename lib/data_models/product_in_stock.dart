815a4e29e1d61765ff60235e29fef215174f5c7c
Duy Le <duy.leduc8@gmail.com>
2019-12-23 02:19:22 +0700

    search engine, phase 1 done!

--- lib/data_models/product_in_stock.dart
+++ lib/data_models/product_in_stock.dart
@@ -4,8 +4,8 @@ class ProductInStock {
   bool status;
   double daily_price;
   double weekly_price;
-  double monthy_price;
-  List<Product_Image_Video> list_img_vid;
+  double monthly_price;
+  //List<Product_Image_Video> list_img_vid;
   int user_id;
 
   ProductInStock(
@@ -14,11 +14,13 @@ class ProductInStock {
       this.status,
       this.daily_price,
       this.weekly_price,
-      this.monthy_price,
-      this.list_img_vid,
+      this.monthly_price,
+      //this.list_img_vid,
       this.user_id});
 }
 
 class Product_Image_Video {
   String img_vid_url;
 }
+
+List<ProductInStock> productInStockList = [];
