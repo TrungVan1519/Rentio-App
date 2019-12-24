ca8fd39dd80c9c315d9a5684502524e1ea1b6a2e
Duy Le <duy.leduc8@gmail.com>
2019-12-24 18:24:40 +0700

    merge 24/09/2019

---
+++ lib/models/product_in_stock.dart
@@ -0,0 +1,26 @@
+class ProductInStock {
+  int id;
+  String name;
+  bool status;
+  double daily_price;
+  double weekly_price;
+  double monthly_price;
+  //List<Product_Image_Video> list_img_vid;
+  int user_id;
+
+  ProductInStock(
+      {this.id,
+      this.name,
+      this.status,
+      this.daily_price,
+      this.weekly_price,
+      this.monthly_price,
+      //this.list_img_vid,
+      this.user_id});
+}
+
+class Product_Image_Video {
+  String img_vid_url;
+}
+
+List<ProductInStock> productInStockList = [];
